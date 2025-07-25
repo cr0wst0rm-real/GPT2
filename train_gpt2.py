568216ef6a7f1f3f1db2110c0488ba9181f3a19d
CitizenCEO <matees228@gmail.com>
2025-07-25 19:31:53 +0200

    Update train_gpt2.py
    - automatic device selection
    - training using logits, entropy loss
    
    1:16:16

--- train_gpt2.py
+++ train_gpt2.py
@@ -87,8 +87,21 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+        #weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
 
-    def forward(self, idx): #idx -> token indices as input
+        # init params
+        self.apply(_init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module. nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None): #idx -> token indices as input
         # idx is of shape (B, T)
         B, T = idx.size()
         assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
@@ -103,7 +116,10 @@ class GPT(nn.Module):
         # forward the final layernorm and the classifier
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x) # (B, T, vocab_size)  this calculates B, T+1 (next token that comes after), vocab size is dimension specifiing number of possible tokens
-        return logits
+        loss=None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1)) # flatten logits first two dimensions to just 1 (cross_entropy does not accept more than 2 dimensions)
+        return logits, loss
 
 
     @classmethod
@@ -157,23 +173,94 @@ class GPT(nn.Module):
 
 
 
-# -------------------------------
-num_return_sequences = 5
-max_length = 30
 
-model = GPT.from_pretrained('gpt2')
-model.eval()
-model.to('cuda')
-print("didn't crash yay!")
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        # at init load tokens from disk and store them in mem
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T)
+        y = (buf[1:]).view(B, T)
+        # advance the position in the tensor
+        self.current_position += B*T
+        if self.current_position + (B*T+1) > len(self.tokens):
+            self.current_position = 0
+        return x,y
+
+
+
+
+
+
+
+
+# --------------------
+# auto get device
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+elif hasattr(torch.backends, "mps"):
+    device = "mps"
+print(f"using device: {device}")
+
+
+
+train_loader = DataLoaderLite(B=4, T=32)
+
+
+# get logits
+model = GPT(GPTConfig())
+model.to(device)
+
+# optimize
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    x,y = train_loader.next_batch()
+    x,y = x.to(device), y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x,y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
+
+
+
+print(loss)
+import sys; sys.exit(0)
+
+
+
+
+
 
 
 #prefix tokens
+model.eval()
+num_return_sequences = 5
+max_length = 30
 import tiktoken
 enc = tiktoken.get_encoding('gpt2')
 tokens = enc.encode("Hello world!")
 tokens = torch.tensor(tokens, dtype=torch.long)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)        # this will replicate the tokens 5 times (num_torch_sequences)
-x = tokens.to('cuda')
+x = tokens.to(device)
 
 
 # generate! right now x is (B, T) where B = 5, T = 8
